f857d5f7c58c7afcdbde90ee18955f0ac696c111
Wil Carmon <java@wcarmon.com>
2024-05-24 18:16:43 -0400

    setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -144,10 +144,7 @@ tasks.javadoc {
 
 configure<SpotlessExtension> {
     java {
-        googleJavaFormat("1.18.1")
-            .skipJavadocFormatting()
-            .aosp()
-            .reflowLongStrings()
+        googleJavaFormat("1.18.1").aosp().reflowLongStrings().skipJavadocFormatting()
         importOrder()
         removeUnusedImports()
 
